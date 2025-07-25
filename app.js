8ff0d29a908577796c27881f33d63d97ecec3f6c
성현 <xpfl7920@gmail.com>
2025-07-26 02:16:30 +0900

    수정

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// 서버에서 클라이언트로부터 전송된 JSON 형식의 데이터를
-// 파싱(해석) 하기 위해 사용하는 미들웨어
+require("dotenv").config();
+
 const index = require("./routes/index");
 
-const port = 5000;
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", index);
 
-// 이렇게 작성시 데이터가 들어오면 자동으로 tode-list가 만들어진다.
-const mongoURI = `mongodb://localhost:27017/todo-list`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
